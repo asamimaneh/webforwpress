3d983c3a0bf439643c5b56afa72b54cc8f7fb1ad
amirmohammad <amirmohamadsamimanh@gmail.cpm>
2024-01-19 15:53:39 +0330

    add code to new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {HeaderComponent} from "./header/header.component";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
