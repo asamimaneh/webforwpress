1680d74b792745582345593020e520f6ecdfdbf8
amirmohammad <amirmohamadsamimanh@gmail.cpm>
2024-01-19 16:59:25 +0330

    add code to new project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {FooterComponent} from "../footer/footer.component";
+import {MainComponent} from "../main/main.component";
+import {AboutComponent} from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    AboutComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
