64a25ae73b18363f5eb800d1e0706dd767dfe5b3
amirmohammad <amirmohamadsamimanh@gmail.cpm>
2024-01-19 21:27:17 +0330

    move code to github

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
