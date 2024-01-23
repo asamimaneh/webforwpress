1966cb949e7dc71e1d1e35081a1b09f525c841c9
amirmohammad <amirmohamadsamimanh@gmail.cpm>
2024-01-23 17:37:25 +0330

    generate component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,18 @@ import {HeaderComponent} from "../header/header.component";
 import {FooterComponent} from "../footer/footer.component";
 import {MainComponent} from "../main/main.component";
 import {AboutComponent} from "../about/about.component";
+import {BlogComponent} from "../blog/blog.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    FooterComponent,
-    MainComponent,
-    AboutComponent
-  ],
+    imports: [
+        HeaderComponent,
+        FooterComponent,
+        MainComponent,
+        AboutComponent,
+        BlogComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
