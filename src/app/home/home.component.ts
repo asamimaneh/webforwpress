fe4b1ddc6f8f3ac3afd8744342b76aba27376fc7
amirmohammad <amirmohamadsamimanh@gmail.cpm>
2024-02-06 21:01:03 +0330

    add navigator to project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,18 +5,20 @@ import {MainComponent} from "../main/main.component";
 import {AboutComponent} from "../about/about.component";
 import {BlogComponent} from "../blog/blog.component";
 import {FormatComponent} from "../format/format.component";
+import {NavigationComponent} from "../navigation/navigation.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        HeaderComponent,
-        FooterComponent,
-        MainComponent,
-        AboutComponent,
-        BlogComponent,
-        FormatComponent
-    ],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AboutComponent,
+    BlogComponent,
+    FormatComponent,
+    NavigationComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
